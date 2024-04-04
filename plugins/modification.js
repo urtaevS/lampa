bb1783e9c63fad186caa7d671f5636f8d2b741b9
bylampa <157304712+bylampa@users.noreply.github.com>
2024-04-04 13:10:45 +0300

    Update modification.js

--- plugins/modification.js
+++ plugins/modification.js
@@ -1,6 +1,6 @@
 (function () {
     'use strict';
     Lampa.Utils.putScriptAsync([
-	    'https://bylampa.github.io/addon.js'
+	    'https://bylampa.github.io/addon.js?v=' + Math.random()
     ], function () {});
 })();
